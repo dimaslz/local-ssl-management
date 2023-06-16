0960e7f06e351ab576d7e949e7077b456ba61c3e
dimaslz <me@dimaslz.io>
2023-06-16 19:07:11 +0200

    fix: allow remove domain by uuid

--- packages/cli/src/on-remove-action.ts
+++ packages/cli/src/on-remove-action.ts
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import shell from "shelljs";
 
-import { Config } from "@dimaslz/local-ssl-management-core";
+import type { Config } from "@dimaslz/local-ssl-management-core";
 import chalk from "chalk";
 import generateProxyImage from "./generate-proxy-image";
 
@@ -14,12 +14,22 @@ const sslPath = `${rootPath}/ssl`;
 const config: Config[] = JSON.parse(fs.readFileSync(configPath, { encoding: "utf8" }) || "[]");
 
 const onRemoveAction = (_domain: string) => {
+	const v4 = new RegExp(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
+	const isUUID = v4.test(_domain);
+
 	const domain = _domain.split("_").join(",");
 
-	const exists = config.some((c) => c.domain === domain);
+	const exists = isUUID
+		? config.some((c) => c.id === _domain)
+		: config.some((c) => c.domain === domain);
 
 	if (!exists) {
-		shell.echo(chalk.red(`\n[Error] - Domain "${domain}" does not exists\n`));
+		if (isUUID) {
+			shell.echo(chalk.red(`\n[Error] - Domain with id "${_domain}" does not exists\n`));
+		} else {
+			shell.echo(chalk.red(`\n[Error] - Domain "${domain}" does not exists\n`));
+		}
+
 		shell.exit(1);
 	}
 
