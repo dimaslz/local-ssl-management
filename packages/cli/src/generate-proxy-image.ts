09fc6b2d915e00b592d3cd6693f153d6245dcbea
dimaslz <me@dimaslz.io>
2024-01-01 11:30:05 +0000

    feat(cli): allow multiple services with same domain

--- packages/cli/src/generate-proxy-image.ts
+++ packages/cli/src/generate-proxy-image.ts
@@ -16,6 +16,13 @@ const rootPath = `${distPath}/.local-ssl-management`;
 const sslPath = `${rootPath}/ssl`;
 
 const generateProxyImage = (config: Config[]) => {
+	if (!config.length) {
+    shell.echo("Does not exists config to create reverse proxy");
+		shell.exit(1);
+
+		return
+	}
+
 	const localhostCertExists = fs.existsSync(sslPath + "/localhost-cert.pem");
 	const localhostKeyExists = fs.existsSync(sslPath + "/localhost-key.pem");
 
@@ -45,16 +52,31 @@ const generateProxyImage = (config: Config[]) => {
 			key: `ssl/${certName}-key.pem`
 		});
 
+		const nginxLocationTpl: string = Templates.nginxLocation;
+
+		// setup location
+		let locationConfigs = c.location.split(",").map((locationConfig) => {
+			return nginxLocationTpl
+					.replace("%LOCATION%", locationConfig)
+					.replace("%LOCAL_IP%", LOCAL_IP)
+		}).join("\n\n");
+
+		// setup ports
+		c.port.split(",").forEach((port) => {
+			locationConfigs = locationConfigs.replace("%PORT%", String(port));
+		});
+
 		return nginxConfServerTpl
 			.replace(/\%APP_DOMAIN\%/g, certName)
 			.replace("%SERVER_NAME%", c.domain.split(",").map(i => i.trim()).join(' '))
-			.replace("%LOCAL_IP%", LOCAL_IP)
-			.replace("%PORT%", String(c.port));
+			.replace("#--server-location--#", locationConfigs);
 	});
 
 	const nginxConfTpl = Templates.nginxConf;
-	const nginxConf = nginxConfTpl.replace(/\t\t#--server-config--#/, `\t\t${serverConfigs.join("\n")}`);
+	const nginxConf = nginxConfTpl
+		.replace(/\t\t#--server-config--#/, `\t\t${serverConfigs.join("\n")}`);
 
+	// console.log("nginxConf", nginxConf)
 	const nginxConfDest = `${rootPath}/nginx.conf`;
 	fs.writeFileSync(nginxConfDest, nginxConf);
 
@@ -73,10 +95,10 @@ COPY ${d.cert} /etc/nginx/`
 
 	shell.exec(
 		`NAME=local-ssl-management && \
-  	docker rm -f $NAME && \
-  	docker rmi -f $NAME && \
-  	docker build --no-cache -t $NAME ${rootPath} && \
-  	docker run --name $NAME -p 80:80 -p 443:443 -d $NAME`,
+		docker rm -f $NAME && \
+		docker rmi -f $NAME && \
+		docker build --no-cache -t $NAME ${rootPath} && \
+		docker run --name $NAME -p 80:80 -p 443:443 -d $NAME`,
 		{ silent: true }
 	);
 
