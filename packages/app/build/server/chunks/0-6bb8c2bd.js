b18ade26c35ce48ea1cac31496e127b57e9539ea
dimaslz <me@dimaslz.io>
2024-02-11 12:56:23 +0100

    chore(landing): create basic landing

--- packages/app/build/server/chunks/0-6bb8c2bd.js
+++
@@ -1,8 +0,0 @@
-const index = 0;
-const component = async () => (await import('./_layout.svelte-361f1383.js')).default;
-const imports = ["_app/immutable/nodes/0.30cd1b6a.js","_app/immutable/chunks/index.9433b6d2.js"];
-const stylesheets = ["_app/immutable/assets/0.7c73d456.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=0-6bb8c2bd.js.map
